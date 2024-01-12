7faa341a815cf19c7c63c92120ad85084a472395
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-01-12 11:27:08 -0800

    Fixed pixal array loop logix to go from top left corner of image towards the right and down.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"image"
 	"log"
 
 	"github.com/disintegration/imaging"
@@ -13,9 +13,30 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to open image: %v", err)
 	}
+	pixelArray := getPixelArray(src)
 
-	imageWidth := src.Bounds().Dx()
-	imageHeight := src.Bounds().Dy()
+}
 
-	fmt.Printf("Image width is: %v\nImage height is %v\n", imageWidth, imageHeight)
+// converts RGBA from uint32 to 8-bit pixel value
+func rgbaToPixel(r, g, b, _ uint32) (uint8, uint8, uint8) {
+	// ignoring the alpha for now!
+	red := uint8(r >> 8)
+	green := uint8(g >> 8)
+	blue := uint8(b >> 8)
+	return red, green, blue
+}
+
+func getImageHeightAndWidth(im image.Image) (int, int) {
+	return im.Bounds().Dx(), im.Bounds().Dy()
+}
+
+// this is the function that will be behind
+func getPixelArray(img image.Image) [][]uint8 {
+	width, height := getImageHeightAndWidth(img)
+	result := make([][]uint8)
+	for i := img.Bounds().Min.X; i < width; i++ {
+		for j := img.Bounds().Min.Y; j < height; j++ {
+
+		}
+	}
 }
