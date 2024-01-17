a2b1678d1886b6cb59eaaeadc1a3f00f82ac59af
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-01-17 00:32:03 -0800

    converted this to a 2 dimensional matrix

--- image_proc/intensityMatrix.go
+++ image_proc/intensityMatrix.go
@@ -10,29 +10,29 @@ const (
 )
 
 // maps (r, g, b) pixel values into a singular (i) intensity value depending on the algorithm specified by algoName
-func GetIntensityMatrix(pixelMatrix [][][]uint8, algoName Intensity) [][][]uint8 {
-	// should intensity matrix be float64???
-	intensityMatrix := [][][]uint8{}
-	var intensity uint32
+func GetIntensityMatrix(pixelMatrix [][][]uint8, algoName Intensity) [][]float64 {
+	// try float64 intensity matrix
+	intensityMatrix := [][]float64{}
+	var intensity float64
 	for i := 0; i < len(pixelMatrix); i++ {
-		row := [][]uint8{}
+		row := []float64{}
 		for j := 0; j < len(pixelMatrix[i]); j++ {
 			a, b, c := pixelMatrix[i][j][0], pixelMatrix[i][j][1], pixelMatrix[i][j][2]
 			// convert to 32 bit for temporary math
 			switch algoName {
 			case "average":
-				intensity = (uint32(a) + uint32(b) + uint32(c)) / uint32(3)
+				intensity = (float64(a) + float64(b) + float64(c)) / float64(3)
 			case "max_min":
-				intensity = (max(uint32(a), uint32(b), uint32(c)) + min(uint32(a), uint32(b), uint32(c))) / 2
+				intensity = (max(float64(a), float64(b), float64(c)) + min(float64(a), float64(b), float64(c))) / 2
 			case "luminosity":
-				temp := 0.21*float32(a) + 0.72*float32(b) + 0.72*float32(c)
-				intensity = uint32(temp)
+				intensity = 0.21*float64(a) + 0.72*float64(b) + 0.72*float64(c)
+				// intensity = float64(temp)
 			default:
 				panic("unknown algorithm name!")
 			}
 
-			// convert back to 8 bit
-			row = append(row, []uint8{uint8(intensity)})
+			// intensity matrix is now 2-dimensional
+			row = append(row, float64(intensity))
 		}
 		intensityMatrix = append(intensityMatrix, row)
 	}
